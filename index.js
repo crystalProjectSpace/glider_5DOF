1a063ac6c660ae04dca362f64b2d7beeef55afbb
crystalProjectSpace <sevmailbox@yandex.ru>
2023-05-21 17:34:54 +0300

    Fixed course angle mismatch in ODE system, moved initial data to separate file

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 'use strict'
-const DT = 0.001
+
 const { Interp1D, Interp2D } = require('./utils.js')
-const { writeFile } = require('node:fs/promises') 
-const g0 = 9.807
+const { writeFile } = require('node:fs/promises')
+const { performance } = require('perf_hooks')
+const { MX, AX, DX, CYA, CXA, MZ, dMZ, dMX } = require('./init_data.json') // импорт ИД
+
+const DT = 0.002 // дефолтный шаг интегрирования по времени
+const g0 = 9.807 // ускорение свободного падения
+const DEFAULT_HEADER = 't[s]\tV[m/s]\tTh[gr]\tPsi[gr]\twZ[gr/s]\twX[gr/s]\tX[m]\tY[m]\tZ[m]\tpitch[gr]\troll[gr]\n' // заголовок таблицы результатов выдачи
+const R2G = 180 / Math.PI // перевод градусы <=> радианы
+const N_VARS = 10 // количество уравнений (3 ускорения + 3 скорости + 2 угл.ускорения + 2 угл.скорости)
 /**
 * @description плотность
 */
@@ -16,9 +23,6 @@ const aSnH = function(H) {
 	if (H < 11500) return 340 - 0.00391 * H
 	return 295
 }
-
-const N_VARS = 10 // количество уравнений (3 ускорения + 3 скорости + 2 угл.ускорения + 2 угл.скорости)
-
 /**
 * @description параметры планера
 */
@@ -42,21 +46,21 @@ const testControls = {
 	t0: 0,
 	pitch: 0, // угол отклонения руля высоты
 	roll: 0, // угол отклонения элеронов
-	setRudder(state, t) { // закон управления рулем высоты
+	setRudder(state, t, dT) { // закон управления рулем высоты
 		const V = state[0]
 		const kEps = this.t0 > 0 ? (state[3] - this._alpha0)/DT : 0
-		if(V < 25) return 0
-		let delta = 1.25 * (state[1] + 3.695/57.3) + 75.5 * kEps
-		
-		if(delta < 0) {
-			if(delta < -15) delta = -15
+		if (V < 25) return 0
+		let delta = 1.25 * (state[1] + 3.695/R2G) + 125.5 * kEps
+		const rudderSpeed = dT * 3600
+		if (delta < 0) {
+			if (delta < -rudderSpeed) delta = -rudderSpeed
 			this.pitch = Math.max(-24.5, this.pitch + delta )
 			this._alpha0 = state[3]
 			this.t0 = t
 			return
 		} 
 
-		if(delta > 15) delta = 15
+		if (delta > rudderSpeed) delta = rudderSpeed
 		this.pitch = Math.min(24.5, this.pitch + delta)
 		this._alpha0 = state[3]
 		this.t0 = t
@@ -69,7 +73,7 @@ const testControls = {
 /**
 * @description Получить производные от линейный и угловых скоростей и перемещений
 */
-const getDerivatives = function(controls, params, state, t) {
+const getDerivatives = function(controls, params, state, t, buffer, dT) {
 	const V = state[0]
 	const Th = state[1]
 	const Psi = state[2]
@@ -84,7 +88,7 @@ const getDerivatives = function(controls, params, state, t) {
 	const QS = 0.5 * RoH(Y) * V * V * params.S_wing
 	const QSB = QS * params.B_chord
 	
-	controls.setRudder(state, t)
+	controls.setRudder(state, t, dT)
 	controls.setAilerons(state, t)
 	
 	const deltaElevator = controls.pitch
@@ -107,18 +111,16 @@ const getDerivatives = function(controls, params, state, t) {
 	const SG = Math.sin(gamma)
 	const CG = Math.cos(gamma)
 	
-	return [
-		-_XA - g0 * STH,
-		(_YA * CG - g0 * CTH) / V,
-		_YA * SG / V,
-		MZ / params.Jz,
-		MX * QSB/ params.Jx,
-		V * CTH * CPH,
-		V * STH,
-		V * STH * SPH,
-		omegaZ,
-		omegaX,
-	]
+	buffer[0] = -_XA - g0 * STH
+	buffer[1] = (_YA * CG - g0 * CTH) / V
+	buffer[2] = _YA * SG / V
+	buffer[3] = MZ / params.Jz
+	buffer[4] = MX * QSB/ params.Jx
+	buffer[5] = V * CTH * CPH
+	buffer[6] = V * STH
+	buffer[7] = V * CTH * SPH
+	buffer[8] = omegaZ
+	buffer[9] = omegaX
 }
 /**
 * @description численное интегрирование (метод Эйлера-Коши второго порядка точности)
@@ -130,7 +132,6 @@ const integrate = function(initialState, params, controls, tauMax, dT) {
 	let V = 0
 	let H = 0
 	let alpha = 0
-	let gamma = 0
 	let Mach = 0
 	const dT_05 = 0.5 * dT
 	const dT_6 = dT * 0.1666666666666666667
@@ -142,14 +143,14 @@ const integrate = function(initialState, params, controls, tauMax, dT) {
 	const _state = new Float64Array(N_VARS)
 	const _res = new Float64Array(N_VARS + 1)
 	const { interpCYA, interpCXA, interpMZ, dMZ_elevator, dMX_aileron } = params
+	const t0 = performance.now()
 	
-	while(tau < tauMax) {
+	while (tau < tauMax) {
 		const state = result[i++].slice(1)
 		
 		V = state[0]
 		H = state[6]
-		alpha = state[8] * 57.3
-		gamma = state[9] * 57.3
+		alpha = state[8] * R2G
 		Mach = V / aSnH(H)
 		
 		interpCYA.checkIndices(Mach, alpha)
@@ -158,19 +159,19 @@ const integrate = function(initialState, params, controls, tauMax, dT) {
 		dMZ_elevator.checkIndex(controls.pitch)
 		dMX_aileron.checkIndex(controls.roll)	
 		
-		K0 = getDerivatives(controls, params, state, tau)
+		getDerivatives(controls, params, state, tau, K0, dT)
 		for(let j = 0; j < N_VARS; j++) {
 			_state[j] = state[j] + dT_05 * K0[j]
 		}
-		K1 = getDerivatives(controls, params, _state, tau)
+		getDerivatives(controls, params, _state, tau, K1, dT)
 		for(let j = 0; j < N_VARS; j++) {
 			_state[j] = state[j] + dT_05 * K1[j]
 		}
-		K2 = getDerivatives(controls, params, _state, tau)
+		getDerivatives(controls, params, _state, tau, K2, dT)
 		for(let j = 0; j < N_VARS; j++) {
 			_state[j] = state[j] + dT_05 * K2[j]
 		}
-		K3 = getDerivatives(controls, params, _state, tau)
+		getDerivatives(controls, params, _state, tau, K3, dT)
 		for(let j = 0; j < N_VARS; j++) {
 			_state[j] = state[j] + dT * K3[j]
 		}
@@ -183,51 +184,11 @@ const integrate = function(initialState, params, controls, tauMax, dT) {
 		}
 		result.push(_res.slice())
 	}
+	const t1 = performance.now() - t0
+	console.log(`calculation time: ${t1.toFixed(3)} ms;`)
 	
 	return result	
 }
-// здесь заканчивается описание служебных функций и начинается описание ИД
-
-const MX = [0, 0.3, 0.5, 0.9]
-const AX = [-30, -20, -10, -8, -6, -4, -2, -1, 0, 1, 2, 4, 6, 8, 10, 20, 30]
-const DX = [-25, -20, -15, -10, -5, -2, -1, 0, 1, 2, 5, 10, 15, 20, 25]
-
-// нагрузка на крыло - 75 кг/м2
-// длина - 0.8 м
-// ЦМ - 0.3 м
-// плечо оперения - 0.75 м
-// плечо крыла - 0.275 м
-// отн.площадь элеронов - 0.1
-// отн.площадь рулей высоты - 0.4
-
-const CYA = [
-	[-2.34009, -1.58131, -0.82254, -0.67079, -0.51903, -0.36728, -0.21552, -0.13965, -0.06377, 0.01211, 0.08799, 0.23974, 0.3915, 0.54325, 0.695, 1.45378, 2.21255 ],
-	[-2.34009, -1.58131, -0.82254, -0.67079, -0.51903, -0.36728, -0.21552, -0.13965, -0.06377, 0.01211, 0.08799, 0.23974, 0.3915, 0.54325, 0.695, 1.45378, 2.21255 ],
-	[-2.34009, -1.58131, -0.82254, -0.67079, -0.51903, -0.36728, -0.21552, -0.13965, -0.06377, 0.01211, 0.08799, 0.23974, 0.3915, 0.54325, 0.695, 1.45378, 2.21255 ],
-	[-2.34009, -1.58131, -0.82254, -0.67079, -0.51903, -0.36728, -0.21552, -0.13965, -0.06377, 0.01211, 0.08799, 0.23974, 0.3915, 0.54325, 0.695, 1.45378, 2.21255 ],
-	[-2.34009, -1.58131, -0.82254, -0.67079, -0.51903, -0.36728, -0.21552, -0.13965, -0.06377, 0.01211, 0.08799, 0.23974, 0.3915, 0.54325, 0.695, 1.45378, 2.21255 ]
-]
-
-
-const CXA = [
-	[0.50002, 0.25604, 0.10648, 0.0879, 0.07309, 0.06206, 0.05481, 0.0526, 0.05133, 0.05101, 0.05164, 0.05571, 0.06357, 0.0752, 0.09061, 0.2243, 0.45241],
-	[0.50002, 0.25604, 0.10648, 0.0879, 0.07309, 0.06206, 0.05481, 0.0526, 0.05133, 0.05101, 0.05164, 0.05571, 0.06357, 0.0752, 0.09061, 0.2243, 0.45241],
-	[0.50002, 0.25604, 0.10648, 0.0879, 0.07309, 0.06206, 0.05481, 0.0526, 0.05133, 0.05101, 0.05164, 0.05571, 0.06357, 0.0752, 0.09061, 0.2243, 0.45241],
-	[0.50002, 0.25604, 0.10648, 0.0879, 0.07309, 0.06206, 0.05481, 0.0526, 0.05133, 0.05101, 0.05164, 0.05571, 0.06357, 0.0752, 0.09061, 0.2243, 0.45241],
-	[0.50002, 0.25604, 0.10648, 0.0879, 0.07309, 0.06206, 0.05481, 0.0526, 0.05133, 0.05101, 0.05164, 0.05571, 0.06357, 0.0752, 0.09061, 0.2243, 0.45241],
-]
-
-
-const MZ = [
-	[2.53222, 1.83047, 1.12872, 0.98837, 0.84802, 0.70767, 0.56732, 0.49715, 0.42697, 0.3568, 0.28662, 0.14627, 0.00592, -0.13443, -0.27478, -0.97652, -1.67827],
-	[2.53222, 1.83047, 1.12872, 0.98837, 0.84802, 0.70767, 0.56732, 0.49715, 0.42697, 0.3568, 0.28662, 0.14627, 0.00592, -0.13443, -0.27478, -0.97652, -1.67827],
-	[2.53222, 1.83047, 1.12872, 0.98837, 0.84802, 0.70767, 0.56732, 0.49715, 0.42697, 0.3568, 0.28662, 0.14627, 0.00592, -0.13443, -0.27478, -0.97652, -1.67827],
-	[2.53222, 1.83047, 1.12872, 0.98837, 0.84802, 0.70767, 0.56732, 0.49715, 0.42697, 0.3568, 0.28662, 0.14627, 0.00592, -0.13443, -0.27478, -0.97652, -1.67827],
-	[2.53222, 1.83047, 1.12872, 0.98837, 0.84802, 0.70767, 0.56732, 0.49715, 0.42697, 0.3568, 0.28662, 0.14627, 0.00592, -0.13443, -0.27478, -0.97652, -1.67827],
-]
-
-const dMZ = [1.06031, 0.88241, 0.7045, 0.5266, 0.34869, 0.24195, 0.20637, 0.17079, 0.13521, 0.09963, -0.00712, -0.18502, -0.36293, -0.54083, -0.71874]
-const dMX= [-0.12687, -0.1015, -0.07612, -0.05075, -0.02537, -0.01015, -0.00507, 0, 0.00507, 0.01015, 0.02537, 0.05075, 0.07612, 0.1015, 0.12687 ]
 
 testParams.interpCYA.init(MX, AX, CYA, 0, 0)
 testParams.interpCXA.init(MX, AX, CXA, 0, 0)
@@ -237,42 +198,63 @@ testParams.dMX_aileron.init(DX, dMX, 0)
 
 const initial_state_test = [
 	225,	//V
-	1/57.3,	//Th
+	1/R2G,	//Th
 	0,		//Psi
 	0,		//epsZ
 	0,		//epsX
 	15,		//X
 	14500,	//Y
 	0,		//Z
-	6.2/57.3,		//alpha
+	6.2/R2G,//alpha
 	0		//gamma
 ]
+
+/**
+ * @description Преобразовать результаты в человеко-читаемый вид и записать в текстовый файл
+ * @param {string} path путь к полученному файлу-результату
+ * @param {Array<Float64Array>} result массив данных расчета
+ * @param {number} sparce скважность выборки записей в результат
+ * @param {boolean} comaSwitch флаг замены точки на запятые (для совместимости с Excel/PlanMaker)
+ */
+const printResult = async function(path, data, sparce, tableHeader = DEFAULT_HEADER, comaSwitch = true) {
+	let result = tableHeader
+	const size = Math.floor(data.length / sparce)
+	let k = 0
+	let tempStr = ''
+	for(let i = 0; i < size; i++) {
+		tempStr = [
+			data[k][0].toFixed(2),
+			data[k][1].toFixed(1),
+			(data[k][2] * R2G).toFixed(2),
+			(data[k][3] * R2G).toFixed(2),
+			(data[k][4] * R2G).toFixed(3),
+			(data[k][5] * R2G).toFixed(3),
+			data[k][6].toFixed(0),
+			data[k][7].toFixed(0),
+			data[k][8].toFixed(0),
+			(data[k][9] * R2G).toFixed(2),
+			(data[k][10] * R2G).toFixed(2),
+		].join('\t') + '\n'
+		if (comaSwitch) tempStr = tempStr.replaceAll('.', ',')
+		result += tempStr
+		k += sparce
+	}
+	try {
+		await writeFile(path, result)
+		console.log(`Trajectory data succesfully written to ${path}`)
+	} catch(err) {
+		console.log(`An error occured while writing results: ${err}`)
+	}
+}
 
 const test_integ = integrate(
 	initial_state_test,
 	testParams,
 	testControls,
-	100.5,
+	120.5,
 	DT
-)
+);
 
-const integ_res = test_integ.reduce((res, row, i) => {
-	if(i % 10 !== 0) return res
-	res += [
-		row[0].toFixed(2),
-		row[1].toFixed(1),
-		(row[2] * 57.3).toFixed(2),
-		(row[3] * 57.3).toFixed(2),
-		(row[4] * 57.3).toFixed(3),
-		(row[5] * 57.3).toFixed(3),
-		row[6].toFixed(0),
-		row[7].toFixed(0),
-		row[8].toFixed(0),
-		(row[9] * 57.3).toFixed(2),
-		(row[10] * 57.3).toFixed(2),
-	].join('\t') + '\n'
-	return res
-}, '')
-
-const txt = integ_res.replaceAll('.', ',')
-writeFile('res.txt', txt)
+(async function() {
+	await printResult('res_1.txt', test_integ, 10)
+})()
